28e2ea7414508a3eab2be82a83c33d011c856906
Alex <alex.augustin.84800@gmail.com>
2020-02-04 15:44:07 +0100

    buttons in admin pages

---
+++ view/templates/admin_buttons.php
@@ -0,0 +1,6 @@
+<div id='admin__buttons'>
+        <a class="admin__buttons__selector" href="?action=nouveau_billet"><button>Écrire un article</button></a>
+        <a class="admin__buttons__selector" href='?action=admin_posts'><button>Administration des chapitres</button></a>
+        <a class="admin__buttons__selector" href="?action=admin_comments"><button>Administration des commentaires</button></a>
+        <a class="admin__buttons__selector" href="?action=deconnexion"><button>Deconnexion</button></a>
+    </div>
