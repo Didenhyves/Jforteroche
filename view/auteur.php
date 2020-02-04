c0c06ed52e3d16963d238cbfdb7cfdc092aa8c5e
Alex <alex.augustin.84800@gmail.com>
2020-02-04 15:40:43 +0100

    articles views

---
+++ view/auteur.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Billet&nbsp;simple&nbsp;pour&nbsp;l'Alaska</title>
+    <link rel="shortcut icon" type="image/png" href="images/logo.png">
+    <!--Ici mettre le logo-->
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <!--Ici mettre le CSS-->
+</head>
+
+<body>
+    <div>
+    <?php require('templates/header.php'); ?>
+        <div class="content" id="accueil">
+            <div id="hero">
+                <h1 id="hero__titre" class="titre">Billet simple pour l'Alaska</h1>
+                <h2 id="hero__sous_titre" class="titre"> A propos de l'auteur </h2>
+            </div>
+        </div>
+    </div>
+    <div class="content" id="auteur">
+        <h2 class="titre_section titre"> Jean Forteroche, qui êtes vous ?  </h2>
+        <div id="auteur__text">
+            <div class="auteur__text__question_reponse">
+                <h3><span class="auteur__text__question_reponse_barre">Question</span> : Jean Forteroche, vous êtes connu pour avoir écrit la foule sentimentale, comment vous est venu cette idée, devenu un chef d'oeuvre ? </h3>
+                <p>D'un regard sur la société lors des protestations d'automne, et de l'amour de la foule pour la foule</p>
+            </div>
+            <div class="auteur__text__question_reponse">
+                <h3><span class="auteur__text__question_reponse_barre">Question</span> : Que vous a apporté ce livre ? </h3>
+                <p>Une connaissance de moi, de ma philosophie et de l'amour humain</p>
+            </div>
+            <div class="auteur__text__question_reponse">
+                <h3><span class="auteur__text__question_reponse_barre">Question</span> : Cette fois ci, vous décidez d'utiliser internet pour publier votre livre, pourquoi ce choix ?</h3>
+                <p>Pour permettre à la nouvelle génération de s'ouvrir à la culture littéraire</p>
+            </div>
+            <div class="auteur__text__question_reponse">
+                <h3><span class="auteur__text__question_reponse_barre">Question</span> : Et pourquoi parler de l'Alaska ? </h3>
+                <p>Je m'y suis rendu pendant un voyage spirituel et j'ai été charmé par ces décors</p>
+            </div>
+            <div class="auteur__text__question_reponse">
+                <h3><span class="auteur__text__question_reponse_barre">Question</span> : N'avez vous pas peur de tomber sur des personnes malhônnetes qui pourrait ruiner votre site ?  </h3>
+                <p>Je suis en relation avec un très bon développeur web, très réactif et interessé par mon travail; il s'appelle : <a href="https://www.alexaugustin.fr">Alex AUGUSTIN</a></p>
+            </div>
+            <div class="auteur__text__question_reponse">
+                <h3><span class="auteur__text__question_reponse_barre">Question</span> : Pourrons nous vous revoir sur un futur projet sur... un autre site internet ? </h3>
+                <p>J'attend de voir les critiques et l'avis des gens. Mais je trouve cette idée séduisante</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ view/chapitres.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Billet&nbsp;simple&nbsp;pour&nbsp;l'Alaska</title>
+    <link rel="shortcut icon" type="image/png" href="images/logo.png">
+    <!--Ici mettre le logo-->
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <!--Ici mettre le CSS-->
+</head>
+
+<body>
+    <div>
+    <?php require('templates/header.php'); ?>
+        <div class="content" id="accueil">
+            <div id="hero">
+                <h1 id="hero__titre" class="titre">Billet simple pour l'Alaska</h1>
+                <h2 id="hero__sous_titre" class="titre"> L'intégrale des chapitres</h2>
+            </div>
+        </div>
+    </div>
+    <div class="content fond_bleu" id="accueil_billet">
+        <h2 class="titre_section titre"> Le livre, chapitres par chapitres </h2>
+        <?php require('templates/articles.php') ?>
+</body>
+
+</html>
